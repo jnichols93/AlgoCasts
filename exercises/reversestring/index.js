12f74838fd8e920320c547eb5c35e0d51c791821
Justin Nichols <51006450+jnichols93@users.noreply.github.com>
2020-05-08 11:46:32 -0500

    Update index.js

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -7,10 +7,34 @@
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
-    let newStr = [];
-    while(newStr < str.length){
-        
+    // Method 3 
+    // turn string to array
+    return str.split('').reduce((reversed, character)=> {
+        return character + reversed;
+    },'');
+    // use reduce helper
     }
-}
 
 module.exports = reverse;
+
+    // Method 1; using built in reverse method
+        //     const arr = str.split('');
+        //     arr.reverse();
+        //     return arr.join('');
+
+    // Method 1.5
+        // return str
+        // .split('')
+        // .reverse()
+        // .join('');++]
+
+    //Method 2 
+    //  create an empty string rev
+    // const rev = '';
+    // for each character in the provided string
+    // for (let char of str){
+    //      // take the char and add to front of list
+    //     rev = char + rev
+    // }
+    //     // return rev
+    //     return rev
